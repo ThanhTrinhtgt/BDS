cd92f75bb968e70f46b2228233d255090bbe2055
Thanh Trịnh <thanhtrinh.tgt@gmail.com>
2023-02-02 09:47:17 +0700

    xu ly autoload file

--- Core/autoLoad.php
+++ Core/autoLoad.php
@@ -2,19 +2,31 @@
 
 function autoload($className)
 {
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className);
+    $sitePath = realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+    $nameParts = explode('\\', $className);
 
-    if (file_exists(dirname(dirname(__DIR__)) . '\\' . $fileName . '.php')) {
-        include_once dirname(dirname(__DIR__)) . '\\' . $fileName . '.php';
+    $filePath = $sitePath;
+
+    foreach ($nameParts as $k => $name) {
+        if (in_array($name, ['BDS'])) {
+            continue;
+        }
+
+        $filePath .= $name;
+
+        if ($k == count($nameParts) - 1) {
+            $filePath .= '.php';
+        } else {
+            $filePath .= DIRECTORY_SEPARATOR;
+        }
+    }
+
+    if (is_readable($filePath)) {
+        include_once($filePath);
+
+        return true;
+    } else {
+        return false;
     }
 }
 
