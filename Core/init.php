012f301a66fa2427ed5247f8c16c14b073c38ebf
Thanh Trịnh <thanhtrinh.tgt@gmail.com>
2023-02-03 11:52:56 +0700

    ajax

--- Core/init.php
+++ Core/init.php
@@ -32,10 +32,23 @@ function SafeData($data, $tp = false)
 	if ($tp) {
 		$data = stripcslashes($data);
 	} else {
-
+		$data = removeXSS($data);
 	}
 
 	return addslashes($data);
+}
+
+function removeXSS($content)
+{
+	$content = preg_replace("#\<script\s*[^\>]*\>(.*?)\<\/script\>#iu", '', $content);
+	$content = preg_replace("#\<iframe\s*[^\>]*\>(.*?)\<\/iframe\>#iu", '', $content);
+	$content = preg_replace("#\<frame\s*[^\>]*\>(.*?)\<\/frame\>#iu", '', $content);
+	$content = preg_replace('#href\s*=\s*\"\s*javascript\s*:\s*(.*?)\s*\"#iu', '', $content);
+	$content = preg_replace('#href\s*=\s*\"\s*tel\s*:\s*(.*?)\s*\"#iu', '', $content);
+	$content = preg_replace('#href\s*=\s*\"\s*mailto\s*:\s*(.*?)\s*\"#iu', '', $content);
+	$content = preg_replace('#src\s*=\s*\"\s*javascript\s*:\s*(.*?)\s*\"#iu', '', $content);
+	
+	return $content;
 }
 
 function vd($value = '', $cont = false)
